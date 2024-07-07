17d703d66f2b76faa625a1c324458229e30ac591
HPx-Chandan <chandan176314@gmail.com>
2024-07-07 15:31:00 +0530

    just a casual try

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-     <h1 className=" bg-red-500 text-4xl text-center ">Welcome to the (25 project) project </h1>
+     <h1 className=" bg-red-500 text-4xl text-center p-[2rem] ">Welcome to the (25 project) project </h1>
     </>
   )
 }
